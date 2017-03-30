c719531b18aeebdd3fd565569c363ede0fb7414e
nadavsinai <ns@nadavsinai.com>
2017-03-30 23:26:32 +0300

    declare our intention - create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PetInputComponent } from './pets/pet-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetInputComponent
   ],
   imports: [
     BrowserModule,
