319a1af983fdd46a248ea22efae9951024dc9340
nadavsinai <ns@nadavsinai.com>
2017-03-30 23:27:24 +0300

    making it work

--- src/app/pets/pet-input.component.ts
+++ src/app/pets/pet-input.component.ts
@@ -3,13 +3,13 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'pet-input',
   template: `
-    <input type="text" placeholder="Pet Name" />
-    <button>Add Pet</button>
+    <input type="text" placeholder="Pet Name" #petInput />
+    <button (click)="addPet(petInput.value)">Add Pet</button>
   `
 })
 export class PetInputComponent {
   addPet(petName) {
-    console.log('Pet Added: ', petName);
+    console.log('Pet Added: ', petName) ;
   }
 
 }
