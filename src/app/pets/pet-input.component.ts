9e47ba14d74a38b64ea32b7d51241ae470753cc3
nadavsinai <ns@nadavsinai.com>
2017-03-30 23:41:01 +0300

    work with the model show benefits and demo problem 1

--- src/app/pets/pet-input.component.ts
+++ src/app/pets/pet-input.component.ts
@@ -1,21 +1,23 @@
 import {Component} from '@angular/core';
 import {PetService} from './pet.service';
+import {PetModel} from './pet.model';
 
 @Component({
   selector: 'pet-input',
   template: `
-    <input type="text" placeholder="Pet Name" #petInput />
+    <input type="text" placeholder="Pet Name" #petInput  [value]="petModel.name"/>
     <button (click)="addPet(petInput.value)">Add Pet</button>
   `
 })
 export class PetInputComponent {
+  petModel = new PetModel('moki');
   petService  :PetService;
   constructor(petService:PetService){
     this.petService = petService;
   }
 
   addPet(petName:string) {
-    this.petService.addPet(petName);
+    this.petService.addPet(this.petModel);
   }
 
 }
