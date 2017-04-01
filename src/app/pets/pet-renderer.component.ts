57fd15e69e10a8cebd76fe04adb9159334b93d21
nadavsinai <ns@nadavsinai.com>
2017-04-01 23:55:54 +0300

    ---exercise 2 ---- countdown component  hooking up to other components (step2)

--- src/app/pets/pet-renderer.component.ts
+++ src/app/pets/pet-renderer.component.ts
@@ -15,13 +15,25 @@ import { PetModel } from './pet.model';
   template: `
     <div class="pet-renderer">
       <span [class.awake]="pet.awake" class="petName">{{pet.name}}</span>
-      <input type="checkbox" [checked]="pet.awake" (change)="awakeChange.emit()">
       <img [src]="pet.imgUrl"/>
+      <div>
+        <small>Time till asleep</small>
+        <countdown [to]="pet.nextFeedAt" (due)="petAwakeToggle()"></countdown>
+      </div>
+      <button (click)="feedPet()">Feed</button>
     </div>`
 })
 export class PetRendererComponent {
   @Input() pet: PetModel;
   @Output() awakeChange = new EventEmitter();
+  @Output() feed = new EventEmitter();
 
+  petAwakeToggle() {
+    this.awakeChange.emit()
+  }
+
+  feedPet() {
+    this.feed.emit(this.pet)
+  }
 
 }
