c5f48ba7d928e626f92b79d29ec63257b7575984
nadavsinai <ns@nadavsinai.com>
2017-04-02 23:44:17 +0300

    adding simple form fields, iterating over enum does not work as enum is an object with no iterator in runtime

--- src/app/pets/pet-input.component.ts
+++ src/app/pets/pet-input.component.ts
@@ -1,24 +1,34 @@
-import {Component} from '@angular/core';
-import {PetService} from './pet.service';
-import {PetModel} from './pet.model';
+import { Component } from '@angular/core';
+import { PetService } from './pet.service';
+import { PetModel, Size } from './pet.model';
 
 @Component({
   selector: 'pet-input',
   template: `
-   <h4>Add Pet</h4> <input type="text" placeholder="Pet Name" [(ngModel)]="petModel.name"/>
+    <h4>Add Pet</h4>
+
+    <input type="text" placeholder="Pet Name" [(ngModel)]="addModel.name"/>
+    <input type="text" placeholder="Favourite Food" [(ngModel)]="addModel.favouriteFoods"/>
+    <select ([ngModel])="addModel.size">
+      <option *ngFor="let size of sizes|iterateObject">{{sizes[size.key]}}
+      </option>
+    </select>
     <button (click)="addPet()">Add Pet</button>
   `
 })
 export class PetInputComponent {
-  petModel = new PetModel('');
-  petService  :PetService;
-  constructor(petService:PetService){
+  sizes = Size;
+  addModel = {name: '', favouriteFoods: '', size: Size.medium};
+  petService: PetService;
+
+  constructor(petService: PetService) {
     this.petService = petService;
   }
 
-  addPet(petName:string) {
-    this.petService.addPet(this.petModel);
-    this.petModel = new PetModel();
+  addPet(petName: string) {
+    let petModel = new PetModel('');
+    Object.assign(petModel, this.addModel);
+    this.petService.addPet(petModel);
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { LetterSelectorComponent } from './letter-selector.component';
+import { IterateObjectPipe } from './iterate-object.pipe';
 
 @NgModule({
   providers: [],
-  declarations: [LetterSelectorComponent],
+  declarations: [IterateObjectPipe, LetterSelectorComponent],
   imports: [CommonModule],
-  exports: [CommonModule, FormsModule, LetterSelectorComponent],
+  exports: [CommonModule, FormsModule, LetterSelectorComponent, IterateObjectPipe],
 })
 export class SharedModule {
 }
