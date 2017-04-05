beebf63e0a12ceccaeafd3b51538d5f7fcbd51df
nadavsinai <ns@nadavsinai.com>
2017-04-05 18:17:44 +0300

    make use of ngForm, make form state simpler demonstrating ngModel with no 2-way binding just to register on parent form, and 1-way binding just to initialize value

--- src/app/pets/pet-input.component.ts
+++ src/app/pets/pet-input.component.ts
@@ -1,35 +1,35 @@
 import { Component } from '@angular/core';
 import { PetService } from './pet.service';
 import { PetModel, Size } from './pet.model';
-const addInitialModel = {name: '', favouriteFoods: '', size: Size.medium};
+import { FormGroup, FormGroupDirective, NgForm } from '@angular/forms';
 @Component({
   selector: 'pet-input',
   template: `
     <h4>Add Pet</h4>
-
-    <input type="text" placeholder="Pet Name" [(ngModel)]="addModel.name"/>
-    <input type="text" placeholder="Favourite Food" [(ngModel)]="addModel.favouriteFoods"/>
-    <select ([ngModel])="addModel.size">
-      <option *ngFor="let size of sizes|iterateObject:true">{{sizes[size.key]}}
-      </option>
-    </select>
-    <button (click)="addPet()">Add Pet</button>
+    <form #petForm="ngForm" (ngSubmit)="addPet(petForm)">
+      <input type="text" name="name" placeholder="Pet Name" ngModel/>
+      <input type="text" name="food" placeholder="Favourite Food" ngModel/>
+      <select [ngModel]="sizes[sizes.medium]" name="size">
+        <option *ngFor="let size of sizes|iterateObject:true">{{sizes[size.key]}}
+        </option>
+      </select>
+      <button type="submit">Add Pet</button>
+    </form>
   `
 })
 export class PetInputComponent {
   sizes = Size;
-  addModel = {...addInitialModel}
   petService: PetService;
 
   constructor(petService: PetService) {
     this.petService = petService;
   }
 
-  addPet() {
-    let petModel = new PetModel('');
-    Object.assign(petModel, this.addModel);
+  addPet(petForm: NgForm) {
+    let petModel = new PetModel(petForm.value.name, true);
+    Object.assign(petModel, {size: this.sizes[petForm.value.size], favouriteFoods: petForm.value.food.split(',')});
     this.petService.addPet(petModel);
-    this.addModel = {...addInitialModel};
+    petForm.resetForm({size: this.sizes[this.sizes.medium]})
   }
 
 }
